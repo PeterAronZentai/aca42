951c20beba035c9dfd0f55e119a6d08b7f4b1852
Peter Zentai <peter.zentai@jaystack.com>
2023-03-12 14:33:51 +0100

    first commit

---
+++ cloudenv/build.sh
@@ -0,0 +1,9 @@
+RESOURCE_GROUP="aca42"
+LOCATION="uksouth"
+ENVIRONMENT="aca42"
+ACR_NAME="aca42"
+
+# az group create --name $RESOURCE_GROUP --location $LOCATION
+# az acr create --resource-group $RESOURCE_GROUP --name $ACR_NAME --sku Basic --admin-enabled true
+# az containerapp env create --resource-group $RESOURCE_GROUP --name $ENVIRONMENT --location $LOCATION
+
